3e45ba6cf4b874309435d267524352e16e197c08
doingnz <richard.scott@healthsystemslimited.com>
2023-12-29 12:51:49 +1300

    Azure Function to save Clima_IotHub event data to Table Storage
    Azure function that reads event data and saves it to TableStorage

---
+++ Source/Azure/ClimaServices/FunctionStoreClimaData.cs
@@ -0,0 +1,63 @@
+using Azure.Messaging.EventHubs;
+using ClimaServices.Model;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json;
+using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage.Table;
+using Azure.Data.Tables;
+
+namespace ClimaServices
+{
+    public class FunctionStoreClimaData
+    {
+        private readonly ILogger<FunctionStoreClimaData> _logger;
+
+        public FunctionStoreClimaData(ILogger<FunctionStoreClimaData> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(FunctionStoreClimaData))] 
+        public async Task Run(
+            [EventHubTrigger(eventHubName: "hsl-meadow-iot-hub", Connection = "ConnectionString_IoTHub")] EventData[] events)
+        {
+            TableServiceClient tableServiceClient = new TableServiceClient(Environment.GetEnvironmentVariable("ConnectionString_Table"));
+            // New instance of TableClient class referencing the server-side table
+            TableClient tableClient = tableServiceClient.GetTableClient(tableName: "clima");
+
+            foreach (EventData @event in events)
+            {
+                _logger.LogInformation($"Event Data: {@event.Data}");
+
+                var success = @event.SystemProperties.TryGetValue("iothub-connection-device-id", out var deviceId);
+                var deviceData = JsonSerializer.Deserialize<ClimaRecordDto>(@event.Data);
+                deviceData.DeviceId = deviceId as string ?? string.Empty;
+
+                await tableClient.CreateIfNotExistsAsync();
+
+                var tableEntity = new ClimaRecordTableEntity
+                {
+                    DateTime = deviceData.DateTime,
+                    DeviceId = deviceData.DeviceId,
+                    PartitionKey = deviceData.DeviceId,
+                    RowKey = $"{deviceData.DeviceId}{@event.EnqueuedTime.UtcTicks}",
+                    
+                    Temperature = deviceData.Temperature,
+                    TotalMemory = deviceData.TotalMemory,
+                    SolarVoltage = deviceData.SolarVoltage,
+                    BatteryVoltage = deviceData.BatteryVoltage,
+                    Humidity = deviceData.Humidity,
+                    Pressure = deviceData.Pressure,
+                    Count = deviceData.Count,
+                };
+
+                // Add new item to server-side table
+                await tableClient.AddEntityAsync<ClimaRecordTableEntity>(tableEntity);
+
+                //yield return deviceData;
+            }
+        }
+    }
+}
---
+++ Source/Azure/ClimaServices/Model/ClimaRecordTableEntity.cs
@@ -0,0 +1,25 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace ClimaServices.Model
+{
+    internal record  ClimaRecordTableEntity : ITableEntity
+    {
+        // ITableEntity
+        public required string PartitionKey { get; set; }
+        public required string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+
+        //User Data
+        public string DeviceId { get; set; }
+        public string DateTime { get; set; }
+        public long Count { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public double Pressure { get; set; }
+        public long TotalMemory { get; set; }
+        public double BatteryVoltage { get; set; }
+        public double SolarVoltage { get; set; }
+    }
+}
