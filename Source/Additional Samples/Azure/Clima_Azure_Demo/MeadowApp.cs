3175751fc7a17e2593c04d017ff93276b1ac0d97
doingnz <richard.scott@healthsystemslimited.com>
2024-02-11 14:28:08 +1300

    Initial Clima Azure Demo
    Starting point for Clima Azure Demo application. Supports OTA for firmware, ResetMeadow command, connect to Wifi, NTP updates, health metrics to Meadow.

---
+++ Source/Additional Samples/Azure/Clima_Azure_Demo/MeadowApp.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Meadow;
+using Meadow.Devices;
+using System.Threading.Tasks;
+using Meadow.Hardware;
+using Meadow.Update;
+using Clima_Azure_Demo.MeadowCommands;
+
+namespace Clima_Azure_Demo
+{
+    public class MeadowApp : App<F7CoreComputeV2>
+    {
+        public override Task Initialize()
+        {
+            Resolver.Log.Info("Initialize...");
+
+            var wifi = Device.NetworkAdapters.Primary<IWiFiNetworkAdapter>();
+            wifi.NetworkConnected += (networkAdapter, networkConnectionEventArgs) =>
+            {
+                Resolver.Log.Info("Joined network");
+                Resolver.Log.Info($"IP Address: {networkAdapter.IpAddress}.");
+                Resolver.Log.Info($"Subnet mask: {networkAdapter.SubnetMask}");
+                Resolver.Log.Info($"Gateway: {networkAdapter.Gateway}");
+            };
+
+            RebootMeadowCommand.Initialise();
+
+            return Task.CompletedTask;
+        }
+
+        public override Task Run()
+        {
+            Resolver.Log.Info("Run...");
+
+            Resolver.Log.Info("Initialise Update Service.");
+
+            var svc = Resolver.Services.Get<IUpdateService>() as Meadow.Update.UpdateService;
+            // Uncomment to clear any persisted update info. This allows installing the same update multiple times, such as you might do during development.
+            // svc.ClearUpdates();
+
+            svc.OnUpdateAvailable += (updateService, info) =>
+            {
+                Resolver.Log.Info("Update available!");
+
+                // Queue update for retrieval "later"
+                Task.Run(async () =>
+                {
+                    await Task.Delay(5000);
+                    updateService.RetrieveUpdate(info);
+                });
+            };
+
+            svc.OnUpdateRetrieved += (updateService, info) =>
+            {
+                Resolver.Log.Info("Update retrieved!");
+
+                Task.Run(async () =>
+                {
+                    await Task.Delay(5000);
+                    updateService.ApplyUpdate(info);
+                });
+            };
+
+            Resolver.Log.Info("Hello, Meadow Core-Compute!");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Source/Additional Samples/Azure/Clima_Azure_Demo/MeadowCommands/RebootMeadowCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Meadow;
+using Meadow.Cloud;
+
+namespace Clima_Azure_Demo.MeadowCommands
+{
+    public class RebootMeadowCommand : IMeadowCommand
+    {
+        public int Seconds { get; set; }
+
+        const int DelayMilliseconds = 1000;
+
+        private static readonly RebootMeadowCommand Instance = new RebootMeadowCommand();
+
+
+        public static void Initialise()
+        {
+            Resolver.CommandService?.Subscribe<RebootMeadowCommand>(Instance.HandleCommand);
+        }
+
+        private void HandleCommand(RebootMeadowCommand command)
+        {
+            Resolver.Log.Info($"Received RebootMeadowCommand command with countdown: {command.Seconds}");
+
+            while (command.Seconds-- > 0)
+            {
+                Resolver.Log.Info($"Remaining {command.Seconds}s...");
+                Thread.Sleep(DelayMilliseconds); // use Thread.Sleep() to stop task switching?
+            }
+
+            Resolver.Device.PlatformOS.Reset();
+        }
+    }
+   
+}
